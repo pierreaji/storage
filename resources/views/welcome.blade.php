5c69f77930c5790c8b0c123986d9dc5f6f45f77d
Pier <alrofiqy@gmail.com>
2022-10-19 10:11:39 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Google Drive Integration</title>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+       <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="/upload" method="post" enctype="multipart/form-data">
+                    <br><br>
+                    @csrf
+                    <input type="file" class="form-control" name="thing">
+                    <br>
+                    <input type="submit" class="btn btn-sm btn-block btn-danger" value="Upload"> 
+                </form>
+            </div>
+        </div>
+       </div>
+    </body>
+</html>
